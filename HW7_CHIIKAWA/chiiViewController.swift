dc9f2b04532ab5a8ae9c8925a84265829fdbe40a
Kate Yeh <kateyehyeh@gmail.com>
2025-04-20 16:52:15 +0800

    Add all updated Swift files and project changes

---
+++ HW7_CHIIKAWA/chiiViewController.swift
@@ -0,0 +1,29 @@
+//
+//  chiiViewController.swift
+//  HW7_CHIIKAWA
+//
+//  Created by Kate Yeh on 2025/3/5.
+//
+
+import UIKit
+
+class chiiViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
